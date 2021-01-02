d13116555779c7c89c12d6c847e57495f0cdee1e
cckn <cckn.dev@gmail.com>
2021-01-02 22:40:57 +0900

    plugins 학습 중
    - BannerPlugin
    - DefinePlugin
    - HtmlWebpackPlugin

--- src/app.js
+++ src/app.js
@@ -4,3 +4,7 @@ import nyancat from "./nyancat.jpg";
 document.addEventListener("DOMContentLoaded", () => {
   document.body.innerHTML = `<img src=${nyancat}></img>`;
 });
+
+console.log(process.env.NODE_ENV);
+console.log(TWO);
+console.log(api.domain);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
 const path = require("path");
+const webpack = require("webpack");
+const childProcess = require("child_process");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const removeNewLine = (buffer) => {
+  return buffer.toString().replace("\n", "");
+};
 
 module.exports = {
   mode: "development",
@@ -26,4 +33,31 @@ module.exports = {
       },
     ],
   },
+
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `
+        Build Date :: ${new Date().toLocaleString()}
+        Commit Version :: ${removeNewLine(
+          childProcess.execSync("git rev-parse --short HEAD")
+        )}
+        Auth.name :: ${removeNewLine(
+          childProcess.execSync("git config user.name")
+        )}
+        Auth.email :: ${removeNewLine(
+          childProcess.execSync("git config user.email")
+        )}
+  `,
+    }),
+    new webpack.DefinePlugin({
+      TWO: JSON.stringify("1+1"),
+      "api.domain": JSON.stringify("https://www.naver.com"),
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      templateParameters: {
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : "프로덕션",
+      },
+    }),
+  ],
 };
